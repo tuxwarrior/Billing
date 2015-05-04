6890751e7c604f3f4a7be76125e9a362a93affb4
Ulises Abel Arias Garcia <ulises@tvc1.tvcnetwork.com>
2015-05-04 16:53:40 -0600

    Agregando codigo para database

--- Billing/datasource.php
+++ Billing/datasource.php
@@ -3,12 +3,29 @@
 $servername='localhost';
 $username='validacion';
 $password='1ngr3s0'; //base de pruebas
+$database='validacion';
 
-$conn = mysqli_connect($servername,$username, $password);
+
+$conn = mysqli_connect($servername,$username, $password,$database);
 
 if(!conn){
     die("fallo conexión: ".mysqli_connect_error());    
 }
+
+$result = $conn->query("SELECT * from ingreso");
+
+print '<table border=1>';
+
+while ($row = $result->fetch_assoc()) {
+    print '<tr>';
+    print '<td>'.$row["usuario"].'</td>';
+    print '<td>'.$row["contrasena"].'</td>';
+    print '</tr>';
+}
+print '</table>';
+
+$result->free();
+
 
 echo "conexión exitosa";
 

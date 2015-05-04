5e2675ef780c8c16a54caafec50b258cbf217e94
Ulises Abel Arias Garcia <ulises@tvc1.tvcnetwork.com>
2015-05-04 14:53:56 -0600

    Primer commit, sitio

---
+++ Billing/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div name="header">
+            <h1>Acceder</h1>
+            <h2>Ingrese usuario y contrase&ntilde;a</h2>
+        </div>
+                <div name="contenido">
+                    <div id="login">
+
+                        <form action="index.php" name="ingreso">
+                            <fieldset>
+                            <strong>Usuario: </strong>
+                            <input type="text" name="usuario">
+                            <br>
+                            <Strong>Contraseña: </strong>
+                            <input type="text" name="password">
+                            <input type="submit" name="btnenviar" value="Entrar">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+        <div name="footer">
+            
+        </div>
+        <?php
+        
+        ?>
+    </body>
+</html>

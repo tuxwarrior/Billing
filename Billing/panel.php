de36bf4853d21d723cc9229a5e808f36243b733c
Ulises Abel Arias Garcia <ulises@tvc1.tvcnetwork.com>
2015-05-06 10:38:19 -0600

    terminado demo de tabla y base de datos

--- Billing/panel.php
+++ Billing/panel.php
@@ -32,14 +32,36 @@ include('CONF/config.inc');
                     <div id="tablita">
                         <table>
                             <tr>
-                            <th>ID</th>
-                            <th>Usuario</th>
-                            <th>Contraseña</th>
-                            <th>Privilegio</th>
-                            <th>Salt</th>
+                            <th>calldate</th>
+                            <th>clid</th>
+                            <th>src</th>
+                            <th>dst</th>
+                            <th>dcontext</th>
+                            <th>channel</th>
+                            <th>dstchannel</th>
+                            <th>lastapp</th>
+                            <th>lastdata</th>
+                            <th>duration</th>
+                            <th>billsec</th>
+                            <th>disposition</th>
+                            <th>amaflags</th>
+                            <th>accountcode</th>
+                            <th>userfield</th>
+                            <th>uniqueid</th>
+                            <th>recvip</th>
+                            <th>useragent</th>
+                            <th>ipsrc</th>
+                            <th>osrc</th>
+                            <th>odst</th>
+                            <th>oint</th>
+                            <th>type</th>
+                            <th>trfcst</th>
+                            <th>trfvnt</th>
+                            <th>cost</th>
+                            <th>sell</th>
                             </tr>
                     <?php
-                        $querydatos= 'SELECT * FROM ingreso';
+                        $querydatos= 'SELECT * FROM cdr limit 30';
 
                         $resultseti=mysqli_query($conn,$querydatos);
                         
@@ -47,11 +69,33 @@ include('CONF/config.inc');
                         
                         while($row=mysqli_fetch_assoc($resultseti)){
                                echo "<tr>";
-                               echo "<td>".$row["idUsuario"]."</td>";
-                               echo "<td>".$row['usuario']."</td>";
-                               echo "<td>".$row['contrasena']."</td>";
-                               echo "<td>".$row['privilegio']."</td>";
-                               echo "<td>".$row['salita']."</td>";
+                               echo "<td>".$row["calldate"]."</td>";
+                               echo "<td>".$row['clid']."</td>";
+                               echo "<td>".$row['src']."</td>";
+                               echo "<td>".$row['dst']."</td>";
+                               echo "<td>".$row['dcontext']."</td>";
+                               echo "<td>".$row['channel']."</td>";
+                               echo "<td>".$row['dstchannel']."</td>";
+                               echo "<td>".$row['lastapp']."</td>";
+                               echo "<td>".$row['lastdata']."</td>";
+                               echo "<td>".$row['duration']."</td>";
+                               echo "<td>".$row['billsec']."</td>";
+                               echo "<td>".$row['disposition']."</td>";
+                               echo "<td>".$row['amaflags']."</td>";
+                               echo "<td>".$row['accountcode']."</td>";
+                               echo "<td>".$row['userfield']."</td>";
+                               echo "<td>".$row['uniqueid']."</td>";
+                               echo "<td>".$row['recvip']."</td>";
+                               echo "<td>".$row['usragent']."</td>";
+                               echo "<td>".$row['ipsrc']."</td>";
+                               echo "<td>".$row['osrc']."</td>";
+                               echo "<td>".$row['odst']."</td>";
+                               echo "<td>".$row['oint']."</td>";
+                               echo "<td>".$row['type']."</td>";
+                               echo "<td>".$row['trfcst']."</td>";
+                               echo "<td>".$row['trfvnt']."</td>";
+                               echo "<td>".$row['cost']."</td>";
+                               echo "<td>".$row['sell']."</td>";
                                echo "</tr>";  
                         }
                         mysqli_free_result($resultseti);

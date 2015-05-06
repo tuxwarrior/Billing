7029ea02672b06c1f6c2b2410873c57cdd93936b
Ulises Abel Arias Garcia <ulises@tvc1.tvcnetwork.com>
2015-05-06 17:01:50 -0600

    Iniciando registro y password

--- Billing/registro.php
+++ Billing/registro.php
@@ -7,6 +7,46 @@ header('Location: index.php');
 
 include('CONF/config.inc');
 
+
+define("PBKDF2_HASH_ALGORITHM", "sha256");
+define("PBKDF2_ITERATIONS", 1000);
+define("PBKDF2_SALT_BYTE_SIZE", 24);
+define("PBKDF2_HASH_BYTE_SIZE", 24);
+
+define("HASH_SECTIONS",4);
+define("HASH_ALGORITHM_INDEX", 0);
+define("HASH_ITERATION_INDEX", 1);
+define("HASH_SALT_INDEX", 2);
+define("HASH_PBKDF2_INDEX", 3);
+
+
+function create_hash($nuevapass)
+{
+    
+    //formato: algoritmo:iteraciones:sal:hash
+    $salt= base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_RANDOM));
+    return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" . $salt . ":" . 
+    base64_encode(pbkdf2(
+            PBKDF2_HASH_ALGORITHM,
+            $nuevapass,
+            $salt,
+            PBKDF2_ITERATIONS,
+            PBKDF2_HASH_BYTE_SIZE,
+            true       
+            ));    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 $query="INSERT INTO ingreso(usuario,contrasena) VALUES('$usuario', '$hashedpass');"
 
 
